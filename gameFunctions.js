6c37971cfba6ab1cd77fa81cfa21e9be800d52f5
Arthur José <arthur.jose27@gmail.com>
2023-06-17 16:14:58 -0300

    Update gameFunctions.js
    - Externalizei as funções e variáveis da função. Fica mais fácil para ser referenciado no código
    - Reorganizei a ordem das funções e coloquei um comentário em cima do que cada uma faz.

--- gameFunctions.js
+++ gameFunctions.js
@@ -1,107 +1,14 @@
 // Game state controla se o jogo iniciou ou não. É alterado por startGame e EndGame
 let gameState = 0;
+
 var chordName, chordPositions;
-var timer = timer();
 
-function clearNotes() { window.clearSelectedNotes(); }
-
-//var pointUser = 0, pointChord
-
-function playNote() {
-  // Lógica para verificar se o acorde veio corretamente entra aqui 
-  // Vai precisar de uma forma de verificar os elementos HTML selecionados em fretboard
-  let notesPressed = window.notesPress;
-
-  if (verifChord(chordPositions, notesPressed)) {
-    calculatePoints()
-    //zera o timer e escolhe outro acorde
-    timer.resetTimer()
-    selectRandomChord()
-    //window.notesPress = []
-    //alert(window.notesPress)
-  }
-
-  clearNotes();
-}
-
-//sistema pontos
-function calculatePoints() {
-  let time = getTimerValue();
-  let timelose = pointTime(time)
-
-  setScoreValue(10 - timelose)
-}
-
-function ToggleScoreDiv() {
-  const scoreContainer = document.getElementById('save-score');
-  const children = scoreContainer.children;
-  let setVal = gameState === 1 ? true : false;
-
-  for (let i = 0; i < children.length; i++) {
-    children[i].hidden = setVal;
-  }
-
-}
-
-//retorna a penalidade por tempo
-function pointTime(time) {
-  const [minutes, seconds] = time.split(":");//vou deixar os minutos só por garatia ne vai q precisa
-  if (seconds >= 25 && seconds <= 30) {
-    return 0;
-  } else if (seconds >= 20 && seconds < 25) {
-    return 1;
-  } else if (seconds >= 15 && seconds < 20) {
-    return 2;
-  } else if (seconds >= 10 && seconds < 15) {
-    return 3;
-  } else if (seconds >= 5 && seconds < 10) {
-    return 4;
-  } else if (seconds >= 0 && seconds < 5) {
-    return 5;
-  }
-}
-
-//soma pontos
-function setScoreValue(value) {
-  let scoreElement = document.getElementById('score');
-  let currentValue = parseInt(scoreElement.textContent);
-
-  if (isNaN(currentValue)) {
-    currentValue = 0;
-  }
-
-  let newValue = currentValue + value;
-  scoreElement.textContent = newValue;
-}
-
-//pega o tempo atual do timer
-function getTimerValue() {
-  // Pega o display do timer
-  let display = document.getElementById('timer');
-
-  // Obtém o valor atual do timer do texto do display
-  let timerValue = display.textContent;
-
-  // Retorna o valor atual do timer
-  return timerValue;
-}
-
-//valida chord
-function verifChord(targetChords, pressedChords) {
-  // Verifica se os arrays têm o mesmo tamanho
-  if (targetChords.length !== pressedChords.length) {
-    return false;
-  }
-
-  // Cria cópias ordenadas dos arrays
-  const sortedTarget = targetChords.slice().sort();
-  const sortedPressed = pressedChords.slice().sort();
-
-  // Verifique se todos os elementos correspondem em ambos os arrays
-  return sortedTarget.every((val, index) => val === sortedPressed[index]);
-}
+let timerDisplay = document.getElementById('timer');
+let minutes, seconds, totalSeconds;
+let interval;
 
 
+// Inicia o jogo
 function startGame() {
   // Só inicia se não tiver iniciado
   let display = document.getElementById('timer')
@@ -113,7 +20,7 @@ function startGame() {
     selectRandomChord();//retorna as posiçãoes das notas
 
     // Inicia o timer
-    timer.startTimer();
+    startTimer();
 
     // Seleciona o botão de play
     let playButton = document.getElementById('play');
@@ -124,6 +31,7 @@ function startGame() {
   }
 }
 
+// Finaliza o jogo
 function endGame() {
   // Só finaliza se não estiver finalizado / aguardando
   if (gameState === 1) {
@@ -141,8 +49,8 @@ function endGame() {
 
     // Inicia um timer de 3 segundos, executa a função dentro dele quando acaba
     setTimeout(() => {
-      clearChord();
-      timer.clearDisplay();
+      clearChordDisplay();
+      clearTimerDisplay();
       // Habilita o botão
       playButton.disabled = false;
       // Devolve a função original do botão, de iniciar o jogo
@@ -151,61 +59,158 @@ function endGame() {
   }
 }
 
-function timer() {
-  let display = document.getElementById('timer');
-  let minutes, seconds, totalSeconds;
-  let interval;
+// Quando o jogo está iniciado, toca as notas pressionadas quando o botão principal é clicado
+function playNote() {
+  // Lógica para verificar se o acorde veio corretamente entra aqui 
+  // Vai precisar de uma forma de verificar os elementos HTML selecionados em fretboard
+  let notesPressed = window.notesPress;
 
-  function declareSeconds() {
-    // Aqui configura o tempo em min e seg de tudo, pra não precisar reescrever em mais de um lugar
-    minutes = 0;
-    seconds = 30;
-    totalSeconds = (minutes % 60) + seconds;
+  if (verifChord(chordPositions, notesPressed)) {
+    calculatePoints()
+    //zera o timer e escolhe outro acorde
+    resetTimer()
+    selectRandomChord()
+    //window.notesPress = []
+    //alert(window.notesPress)
   }
 
-  function updateDisplay() {
-    const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    display.textContent = formattedTime;
-  }
-
-  function clearDisplay() {
-    display.textContent = "";
-  }
-
-  function startTimer() {
-    declareSeconds();
-    updateDisplay();
-    interval = setInterval(() => {
-
-      if (totalSeconds <= 0) {
-        clearInterval(interval);
-        endGame();
-        return;
-      }
-
-      totalSeconds--;
-      minutes = Math.floor(totalSeconds / 60);
-      seconds = totalSeconds % 60;
-
-      updateDisplay();
-    }, 1000);
-  }
-
-  function resetTimer() {
-    clearInterval(interval);
-    declareSeconds();
-    updateDisplay();
-    startTimer();
-  }
-
-  function stopTimer() {
-    clearInterval(interval);
-  }
-
-  return { startTimer, resetTimer, stopTimer, clearDisplay };
+  clearNotes();
 }
 
-// Só trouxe a função do Andrei pra cá, talvez ela possa continuar em app.js e ser chamada de lá, não sei...
+// Atribuição dos valores minutes, seconds e totalSeconds (para iniciar ou reiniciar o timer)
+function declareSeconds() {
+  // Aqui configura o tempo em min e seg de tudo, pra não precisar reescrever em mais de um lugar
+  minutes = 0;
+  seconds = 30;
+  totalSeconds = (minutes % 60) + seconds;
+}
+
+// Atualiza o display do Timer
+function updateTimerDisplay() {
+  const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  timerDisplay.textContent = formattedTime;
+}
+
+// Limpa o display do Timer
+function clearTimerDisplay() {
+  timerDisplay.textContent = "";
+}
+
+// Inicia o timer
+function startTimer() {
+  declareSeconds();
+  updateTimerDisplay();
+  interval = setInterval(() => {
+
+    if (totalSeconds <= 0) {
+      clearInterval(interval);
+      endGame();
+      return;
+    }
+
+    totalSeconds--;
+    minutes = Math.floor(totalSeconds / 60);
+    seconds = totalSeconds % 60;
+
+    updateTimerDisplay();
+  }, 1000);
+}
+
+// Reseta o timer
+function resetTimer() {
+  clearInterval(interval);
+  declareSeconds();
+  updateTimerDisplay();
+  startTimer();
+}
+
+// Para o timer
+function stopTimer() {
+  clearInterval(interval);
+}
+
+// Limpa as cordas selecionadas (função vêem de app.js)
+function clearNotes() { window.clearSelectedNotes(); }
+
+// Calcula os pontos que o player ganha ao acertar a nota. Pontos são descontados quanto menor o tempo restante
+function calculatePoints() {
+  let time = getTimerValue();
+  let timelose = pointTime(time)
+
+  setScoreValue(10 - timelose)
+}
+
+// Função para esconder o cadastro do usuário durante o jogo, e aparecer apenas quando acabar(ainda fazendo)
+// function ToggleScoreDiv() {
+//   const scoreContainer = document.getElementById('save-score');
+//   const children = scoreContainer.children;
+//   let setVal = gameState === 1 ? true : false;
+
+//   for (let i = 0; i < children.length; i++) {
+//     children[i].hidden = setVal;
+//   }
+
+// }
+
+// Retorna o valor da penalidade que será descontada da pontuação após o jogador acertar a nota
+function pointTime(time) {
+  const [minutes, seconds] = time.split(":");//vou deixar os minutos só por garatia ne vai q precisa
+  if (seconds >= 25 && seconds <= 30) {
+    return 0;
+  } else if (seconds >= 20 && seconds < 25) {
+    return 1;
+  } else if (seconds >= 15 && seconds < 20) {
+    return 2;
+  } else if (seconds >= 10 && seconds < 15) {
+    return 3;
+  } else if (seconds >= 5 && seconds < 10) {
+    return 4;
+  } else if (seconds >= 0 && seconds < 5) {
+    return 5;
+  }
+}
+
+// Soma os pontos com a quantidade que o jogador acumulou até o momento
+function setScoreValue(value) {
+  let scoreElement = document.getElementById('score');
+  let currentValue = parseInt(scoreElement.textContent);
+
+  if (isNaN(currentValue)) {
+    currentValue = 0;
+  }
+
+  let newValue = currentValue + value;
+  scoreElement.textContent = newValue;
+}
+
+// Pega o tempo atual do timer
+function getTimerValue() {
+  // Pega o display do timer
+  let display = document.getElementById('timer');
+
+  // Obtém o valor atual do timer do texto do display
+  let timerValue = display.textContent;
+
+  // Retorna o valor atual do timer
+  return timerValue;
+}
+
+// Valida se as cordas pressionadas no braço do violão são as da nota alvo
+function verifChord(targetChords, pressedChords) {
+  // Verifica se os arrays têm o mesmo tamanho
+  if (targetChords.length !== pressedChords.length) {
+    return false;
+  }
+
+  // Cria cópias ordenadas dos arrays
+  const sortedTarget = targetChords.slice().sort();
+  const sortedPressed = pressedChords.slice().sort();
+
+  // Verifique se todos os elementos correspondem em ambos os arrays
+  return sortedTarget.every((val, index) => val === sortedPressed[index]);
+}
+
+// Seleciona uma corda aleatória para o jogador tocar
 function selectRandomChord() {
   fetch('chords.json')
     .then(response => response.json())
@@ -235,6 +240,7 @@ function selectRandomChord() {
     });
 }
 
-function clearChord() {
+// Limpa o display de acorde alvo
+function clearChordDisplay() {
   document.getElementById("chords").textContent = '';
 }
